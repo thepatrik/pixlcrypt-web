fe76c525ef79020731b14bb62ab03656f44970fc
Patrik Palmér <patrik.palmer@gmail.com>
2018-06-09 20:56:06 +0200

    Adds a simple progress bar to upload page
    Progress bar (as a line) added during upload
    process.

--- src/Upload.js
+++ src/Upload.js
@@ -4,6 +4,7 @@ import Req from "./utilities/req";
 import Auth from "./utilities/auth";
 import axios from "axios";
 import async from "async";
+import { Line } from "rc-progress";
 import { Notification } from "react-notification";
 
 const CONCURRENCY_LIMIT = 8;
@@ -12,22 +13,38 @@ class Upload extends Component {
 
     constructor(props) {
         super(props);
+        this.progress = {
+            done: 0,
+            total: 0
+        };
         this.state = {
             selectedFile: "",
             isNoticationActive: false,
-            message: ""
+            message: "",
+            percent: 0
         };
         this.auth = new Auth();
     }
 
+    _tick(reset) {
+        this.progress.done = reset ? 0 : this.progress.done + 1;
+        const percent = (this.progress.done / this.progress.total) * 100;
+        this.setState({percent: percent});
+    }
+
     render() {
         return (
-            <div style={{margin: "1rem"}}>
-                <Dropzone accept="image/jpeg, image/png, image/gif, image/bmp, image/tiff" onDrop={this.onDrop.bind(this)}>
-                    <p style={{margin: "0.5rem"}}>Drop your files here or click to select.</p>
-                    <br/>
-                    <p style={{margin: "0.5rem", color: "grey"}}>Only image files (jpeg, png, gif, bmp, tiff) are accepted.</p>
-                </Dropzone>
+            <div>
+                <div style={{margin: "1rem 1rem 0.5rem 1rem"}}>
+                    <Line percent={this.state.percent} strokeWidth="1" trailWidth="1" strokeColor="#f600ff" strokeLinecap="round"/>
+                </div>
+                <div style={{margin: "1rem"}}>
+                    <Dropzone accept="image/jpeg, image/png, image/gif, image/bmp, image/tiff" onDrop={this.onDrop.bind(this)}>
+                        <p style={{margin: "0.5rem"}}>Drop your files here or click to select.</p>
+                        <br/>
+                        <p style={{margin: "0.5rem", color: "grey"}}>Only image files (jpeg, png, gif, bmp, tiff) are accepted.</p>
+                    </Dropzone>
+                </div>
                 <Notification
                     title={"File uploads"}
                     isActive={this.state.isNoticationActive}
@@ -44,6 +61,8 @@ class Upload extends Component {
             isNoticationActive: true,
             message: "Uploading " + files.length + " " + (files.length === 1 ? "file" : "files") + "..."
         });
+        this.progress.total = files.length;
+        this._tick(true);
         async.mapLimit(files, CONCURRENCY_LIMIT, (file, done) => {
             const url = this._getFilePath(file.name);
             const presignUrl = "https://api.pixlcrypt.com/presign?url=" + url + "&operation=putObject";
@@ -52,13 +71,16 @@ class Upload extends Component {
                 const presigned = res.data.presigned;
                 axios.put(presigned, file).then(res => {
                     console.log("File successfully uploaded!", res);
+                    this._tick();
                     done();
                 }).catch(err => {
                     console.log(err);
+                    this._tick();
                     done();
                 });
             }).catch(err => {
                 console.log(err);
+                this._tick();
                 done();
             });
         }, () => {
