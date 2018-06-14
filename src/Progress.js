f5b8eb4ab2d7b2e0f68aef6393cd0d685743645e
Patrik Palmér <patrik.palmer@gmail.com>
2018-06-14 11:49:43 +0200

    New spinner graphics

--- src/Progress.js
+++ src/Progress.js
@@ -1,16 +1,27 @@
-import React, { Component } from "react";
-import loadingImage from "./images/loading.svg";
+import {Spinner} from "spin.js";
+import React, {Component} from "react";
 import "./Progress.css";
 
-class Progress extends Component {
+export default class Progress extends Component {
+
+    componentDidMount() {
+        const opts = {
+            lines: 12,
+            length: 20,
+            width: 10,
+            radius: 18,
+            corners: 1
+        };
+
+        this.spinner = new Spinner(opts);
+        this.spinner.spin(this.refs.container);
+    }
+
+    componentWillUnmount() {
+        this.spinner.stop();
+    }
 
     render() {
-        return (
-            <div id="loading">
-                <img src={loadingImage} alt="loading"/>
-            </div>
-        );
+        return <span ref="container"/>;
     }
 }
-
-export default Progress;
