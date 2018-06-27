a20ed5b369988de9ef6aa166c3732cefbcc5bc36
Patrik Palmér <patrik.palmer@gmail.com>
2018-06-27 09:10:33 +0200

    Adds a basic video player
    Access video player at route /player

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Auth from "./utilities/auth.js";
 import AuthControl from "./AuthControl.js";
 import Upload from "./Upload.js";
 import Grid from "./Grid.js";
+import Player from "./Player.js";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 class App extends Component {
@@ -39,6 +40,12 @@ class App extends Component {
                         render={() => {
                             return (
                                 <SignoutControl />
+                            );
+                        }}/>
+                    <Route path="/player"
+                        render={() => {
+                            return (
+                                <Player />
                             );
                         }}/>
                     <Route path="/upload"
---
+++ src/Player.js
@@ -0,0 +1,13 @@
+import "../node_modules/video-react/dist/video-react.css";
+import React from "react";
+import { Player } from "video-react";
+
+export default () => {
+    return (
+        <Player
+            playsInline
+            autoPlay
+            src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
+        />
+    );
+};
